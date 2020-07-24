db609a4d7342c7fe64733898191cfe02100a7190
Ragib <ragib08@gmail.com>
2020-07-24 22:26:48 +0600

    Added Layout,Auxilary and body

---
+++ src/components/Body/Body.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+
+import Auxilary from '../../hoc/Auxilary';
+
+class Body extends Component
+{
+    render()
+    {
+        return(
+            <Auxilary>
+            
+             <div>Burger Preview</div>
+             <div>Burger Ingredients</div>
+             <div>Burger Buttons</div>
+            
+
+            </Auxilary>
+        );
+    }
+}
+
+export default Body;
